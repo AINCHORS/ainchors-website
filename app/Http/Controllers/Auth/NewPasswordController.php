059f940c70d61db2dceadc34066892db94cc5f5f
AINCHORS <info@ainchors.com>
2026-09-04 16:51:57 +0800

    fix: clear forced change flag after email password reset

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -34,6 +34,7 @@ class NewPasswordController extends Controller
                 $user->forceFill([
                     'password' => Hash::make($password),
                     'remember_token' => Str::random(60),
+                    'must_change_password' => false,
                 ])->save();
 
                 event(new PasswordReset($user));
